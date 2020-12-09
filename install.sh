5ecc87ef931527ec2a457da24d1badd7a8f5bcd7
Arne Molland <arne@molland.sh>
2020-12-09 09:40:28 +0100

    Qnd fix for interactive script

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 # Used in GitHub Codespaces and other development containers
-case `uname` in
-  Darwin)
-    # commands for OS X go here
-    xcode-select --install || true
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+case $(uname) in
+Darwin)
+  # commands for OS X go here
+  xcode-select --install || true
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
   ;;
-  Linux)
-    # commands for Linux go here
-    sudo apt update
-    curl -fsSL https://starship.rs/install.sh | bash
+Linux)
+  # commands for Linux go here
+  sudo apt update
+  yes | curl -fsSL https://starship.rs/install.sh | bash
   ;;
 esac
