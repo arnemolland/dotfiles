26b78d2f0460a95f69890110e3d52c4b2644e5c9
Arne Molland <arne@molland.sh>
2021-02-04 17:50:01 +0100

    Update font script

--- fonts.sh
+++ fonts.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+brew install fontconfig wget
 
 cd /tmp
 
@@ -7,12 +8,25 @@ wget "https://github.com/IBM/type/archive/master.zip"
 unzip master.zip
 rm master.zip
 
-cd type-master
+cd plex-master
 
-sudo mkdir -p /usr/share/fonts/truetype/ibm-plex
-sudo mkdir -p /usr/share/fonts/opentype/ibm-plex
 
-sudo cp /tmp/type-master/fonts/*/desktop/pc/*.ttf /usr/share/fonts/truetype/ibm-plex/.
-sudo cp /tmp/type-master/fonts/*/desktop/mac/*.otf /usr/share/fonts/opentype/ibm-plex/.
+
+case `uname` in
+  Darwin)
+    yes | sudo cp /tmp/plex-master/fonts/*/desktop/mac/*.otf /Library/Fonts/.
+  ;;
+  Linux)
+    sudo mkdir -p /usr/share/fonts/truetype/ibm-plex
+    sudo mkdir -p /usr/share/fonts/opentype/ibm-plex
+    sudo cp /tmp/plex-master/fonts/*/desktop/pc/*.ttf /usr/share/fonts/truetype/ibm-plex/.
+    sudo cp /tmp/plex-master/fonts/*/desktop/mac/*.otf /usr/share/fonts/opentype/ibm-plex/.
+  ;;
+esac
+
+
+
+
+
 
 sudo fc-cache -fv
