d62d068374b39022b25ab35b16910b2a8490a4db
Arne Molland <arne@molland.sh>
2022-01-03 13:13:56 +0100

    Update Linux packages

--- fresh_install.sh
+++ fresh_install.sh
@@ -22,10 +22,9 @@ case `uname` in
     sudo apt-get install apt-transport-https
     sudo sh -c 'wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -'
     sudo sh -c 'wget -qO- https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'
-    apt-get update
-    apt-get install dart
-    apt install $(<apt/packages.txt)
-    snap install slack code android-studio kubectl flutter --classic
+    sudo apt-get install -y dart
+    sudo apt install -y $(<apt/packages.txt)
+    sudo snap install slack code android-studio kubectl flutter --classic
   ;;
 esac
 
