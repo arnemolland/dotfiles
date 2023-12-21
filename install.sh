1c0517a87e5bcff7a22ce41480c71c0aacbaf1f4
Arne Molland <arne@molland.sh>
2023-12-21 10:21:58 +0100

    install: download zsh plugins

--- install.sh
+++ install.sh
@@ -20,8 +20,13 @@ create_symlinks() {
 create_symlinks
 
 oh_my_zsh() {
+    export ZSH_CUSTOM="$ZSH/custom"
     sh -c "$(curl -sS https://starship.rs/install.sh)" -y -f
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+    git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git $ZSH_CUSTOM/plugins/fast-syntax-highlighting
+    git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
 }
 
 oh_my_zsh
