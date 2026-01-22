5687b56dbe73e139529c77dea6b7d7d1ac739242
Arne Molland <arne@molland.sh>
2026-01-22 10:40:35 +0100

    nix/pkgs/dbpro: fix url callback handling

--- nix/pkgs/dbpro.nix
+++ nix/pkgs/dbpro.nix
@@ -21,8 +21,13 @@ appimageTools.wrapType2 {
   extraInstallCommands = ''
     desktopFile=$(find ${appimage} -maxdepth 1 -name "*.desktop" -print -quit)
     if [ -n "$desktopFile" ]; then
-      install -m 444 -D "$desktopFile" $out/share/applications/${pname}.desktop
-      sed -i "s|^Exec=.*|Exec=${pname}|" $out/share/applications/${pname}.desktop
+      tmpDesktop=$(mktemp)
+      cp "$desktopFile" "$tmpDesktop"
+      sed -i "s|^Exec=.*|Exec=${pname} %U|" "$tmpDesktop"
+      if ! grep -q '^TryExec=' "$tmpDesktop"; then
+        printf '%s\n' "TryExec=${pname}" >> "$tmpDesktop"
+      fi
+      install -m 444 -D "$tmpDesktop" $out/share/applications/${pname}.desktop
     fi
 
     pngIcon=$(find ${appimage} -maxdepth 1 -name "*.png" -print | sort | tail -n1)
@@ -34,6 +39,22 @@ appimageTools.wrapType2 {
     if [ -n "$svgIcon" ]; then
       install -m 444 -D "$svgIcon" $out/share/icons/hicolor/scalable/apps/${pname}.svg
     fi
+
+    mkdir -p $out/share/kservices6
+    cat > $out/share/kservices6/${pname}.protocol <<'PROTO'
+[Protocol]
+protocol=dbpro
+exec=dbpro %u
+input=none
+output=none
+helper=true
+listing=false
+reading=false
+writing=false
+makedir=false
+deleting=false
+Icon=dbpro
+PROTO
   '';
 
   meta = with lib; {
