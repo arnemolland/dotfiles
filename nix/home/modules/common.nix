e5d7000d6235939bc47c0facbb1f61085b1f62f3
Arne Molland <arne@molland.sh>
2026-03-01 17:05:17 +0100

    fix deprecated home-manager option warnings
    - Rename dockerfile-language-server-nodejs to dockerfile-language-server
    - Migrate git userName/userEmail/extraConfig to programs.git.settings

--- nix/home/modules/common.nix
+++ nix/home/modules/common.nix
@@ -47,7 +47,7 @@ in
       rust-analyzer
       zls
       sqls
-      dockerfile-language-server-nodejs
+      dockerfile-language-server
       lua-language-server
       nodePackages.typescript-language-server
       pyright
